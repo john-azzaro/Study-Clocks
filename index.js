57ee18c8de2ebcb4715e8c0e82a5fc03144c4774
John Azzaro <jazzaro@temple.edu>
2019-07-12 14:07:51 -0700

    resolved setClock function issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-setInterval(setClock, 1000)                                            // first, use setInterval to call "setClock" every 1000 milliseconds.
+setInterval(setClock, 1000);                                           // first, use setInterval to call "setClock" every 1000 milliseconds.
 
 const hourHand = document.querySelector('[hour-hand]');                // Query selector for data attribute hour-hand
 const minuteHand = document.querySelector('[minute-hand]');            // Query selector for data attribute minute-hand
@@ -10,5 +10,12 @@ function setClock() {
     const currentDate = new Date();                                     // to get current date, call new Date constructor.
     const seconds = currentDate.getSeconds() / 60;                      // to get seconds, take currentDate.getSeconds and since 60 seconds in a minute, divide by 60.
     const minutes = (seconds + currentDate.getMinutes()) / 60;          // To have minute hand gradually move between minutes on clock, have minutes equal seconds plus current minute              
-    const hours = (minutes + currentDate.getHours()) / 12;              // To have the hour hand grdually move around the clock, have hours equal minutes plus         
+    const hours = (minutes + currentDate.getHours()) / 12;              // To have the hour hand grdually move around the clock, have hours equal minutes plus  
+    setClockRotation(secondHand, seconds);       
+    setClockRotation(minuteHand, minutes);       
+    setClockRotation(hourHand, hours);       
 }
+
+function setClockRotation(element, rotation) {                                   // To set the hands to the proper times. which take the element we want to rotate and the rotation we want to use:
+    element.style.setProperty('--rotation', rotation * 360)               // so take the element you want, take the style of that element, and call the setProperty method which take the property we want ot set (i.e. --rotation) and set to rotation of 360 degrees for the complete rotation.
+}             
