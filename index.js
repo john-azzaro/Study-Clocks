ecd46faacb83a85d011b15e55af26e246b489c84
John Azzaro <jazzaro@temple.edu>
2019-07-12 13:37:43 -0700

    setClock JS issue unresolved

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+
+setInterval(setClock, 1000)                                             // first, use setInterval to call "setClock" every 1000 milliseconds.
+
+function setClock() {
+    const currentDate = new Date();                                     // to get current date, call new Date constructor.
+    const seconds = currentDate.getSeconds() / 60;                      // to get seconds, take currentDate.getSeconds and since 60 seconds in a minute, divide by 60.
+    const minutes = (seconds + currentDate.getMinutes()) / 60;          // To have minute hand gradually move between minutes on clock, have minutes equal seconds plus current minute              
+    const hours = (minutes + currentDate.getHours()) / 12;              // To have the hour hand grdually move around the clock, have hours equal minutes plus         
+}
