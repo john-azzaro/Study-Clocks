1e53c0df07dad29a18c9f3cecfd4d4dc8104eb25
John Azzaro <jazzaro@temple.edu>
2019-10-03 21:19:30 -0700

    improved js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-setInterval(setClock, 1000);                                           // first, use setInterval to call "setClock" every 1000 milliseconds.
-
 const hourHand = document.querySelector('[hour-hand]');                // Query selector for data attribute hour-hand
 const minuteHand = document.querySelector('[minute-hand]');            // Query selector for data attribute minute-hand
 const secondHand = document.querySelector('[second-hand]');            // Query selector for data attribute second-hand
@@ -19,3 +17,13 @@ function setClock() {
 function setClockRotation(element, rotation) {                           // To set the hands to the proper times. which take the element we want to rotate and the rotation we want to use:
     element.style.setProperty('--rotation', rotation * 360)              // so select the element you want, take the style of that element, and call the setProperty method which take the property we want ot set (i.e. --rotation) and set to rotation of 360 degrees for the complete rotation.
 }             
+
+function runClock() {                                                    // When runClock is run...
+    setInterval(setClock, 1000);                                         //  ... use setInterval to call "setClock" every 1000 milliseconds.
+}
+
+function initialize() {                                                  // When the app is initialized, run runClock.
+    runClock();
+}
+
+$(initialize);                                                           // initialize the app to run on start.
